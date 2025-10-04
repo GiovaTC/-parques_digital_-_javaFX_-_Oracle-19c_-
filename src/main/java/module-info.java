537ba095bce6c2a729de2c3135bd98d9c9ca30da
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-04 10:54:52 -0500

    commit - initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.parquesdigital {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.parquesdigital to javafx.fxml;
+    exports com.example.parquesdigital;
+}
