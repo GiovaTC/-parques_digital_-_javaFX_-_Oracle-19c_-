1cde915f50257a28c0abcd9b71e324e2c59e9445
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-04 11:04:18 -0500

    tercer - commit

---
+++ src/main/java/com/example/parquesdigital/MainApp.java
@@ -0,0 +1,13 @@
+package com.example.parquesdigital;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
