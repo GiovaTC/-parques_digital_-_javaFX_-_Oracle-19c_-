7070ce783756a198cba372366ac0e63a1e74df32
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-04 12:30:50 -0500

    commit - module-info.java :.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,13 @@ module com.example.parquesdigital {
     requires org.controlsfx.controls;
     requires java.sql;
 
+    // 👇 Paquete raíz abierto a FXML
     opens com.example.parquesdigital to javafx.fxml;
+
+    // 👇 Paquete de controladores abierto a FXML
+    opens com.example.parquesdigital.controller to javafx.fxml;
+
+    // Exporta los paquetes que quieras usar fuera del módulo
     exports com.example.parquesdigital;
+    exports com.example.parquesdigital.controller;
 }
