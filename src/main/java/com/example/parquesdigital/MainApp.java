6142700e0fb90d27d7433269cf20bab949941a14
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-04 11:20:35 -0500

    commit - MainApp.java

--- src/main/java/com/example/parquesdigital/MainApp.java
+++ src/main/java/com/example/parquesdigital/MainApp.java
@@ -2,12 +2,22 @@ package com.example.parquesdigital;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/game.fml"));
+        Scene scene = new Scene(loader.load(), 800, 600);
+        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+        stage.setTitle("parques digital");
+        stage.setScene(scene);
+        stage.show();
+    }
 
+    public static void main(String[] args) {
+        launch();
     }
 }
