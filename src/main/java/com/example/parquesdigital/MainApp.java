4d4d21b1921b924014006741d2e869433973490b
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-04 12:23:03 -0500

    commit - game.fxml, styles.css :.

--- src/main/java/com/example/parquesdigital/MainApp.java
+++ src/main/java/com/example/parquesdigital/MainApp.java
@@ -5,14 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/game.fml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/game.fxml"));
         Scene scene = new Scene(loader.load(), 800, 600);
         scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
-        stage.setTitle("parques digital");
+        stage.setTitle("🎲 Parques Digital");
         stage.setScene(scene);
         stage.show();
     }
@@ -21,3 +20,4 @@ public class MainApp extends Application {
         launch();
     }
 }
+
